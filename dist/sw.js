e3d400c28f2d8c72d118725c5e548a738a4980c1
Aphel234 <luftlinie.kichern5u@icloud.com>
2026-08-20 11:45:15 +0200

    Add files via upload

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "workshop-zuteilung-v1";
+const CACHE = "workshop-zuteilung-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./assets/app.js", "./favicon.svg", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
