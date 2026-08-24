9952c48d86e76d3d05477cadeaef035638268a1c
Aphel234 <luftlinie.kichern5u@icloud.com>
2026-08-24 16:01:21 +0200

    Add files via upload

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "workshop-zuteilung-v7d-vollstaendige-importvorlage";
-const ASSETS = ["./", "./index.html", "./styles.css", "./assets/app.js", "./assets/jszip.min.js", "./assets/xlsx-import-fix.js", "./assets/project-delete.js", "./favicon.svg", "./manifest.webmanifest"];
+const CACHE = "workshop-zuteilung-v8-klasse-gr-erhalten";
+const ASSETS = ["./", "./index.html", "./styles.css", "./assets/app.js", "./assets/jszip.min.js", "./assets/xlsx-import-fix.js", "./assets/project-delete.js", "./assets/survey-import.js", "./favicon.svg", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => {
