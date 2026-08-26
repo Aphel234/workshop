fdec4909d3c3c4e94e0a37dca72e404910062110
Aphel234 <luftlinie.kichern5u@icloud.com>
2026-08-26 17:04:13 +0200

    Add files via upload

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "workshop-zuteilung-v9-3-0-debattiergruppen-patch1";
+const CACHE = "workshop-zuteilung-v9-3-1-jahrgangstausch";
 const ASSETS = ["./", "./index.html", "./styles.css", "./assets/app.js", "./assets/jszip.min.js", "./assets/xlsx-import-fix.js", "./assets/survey-import.js", "./assets/table-sort.js", "./assets/attendance-check.js", "./favicon.svg", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
